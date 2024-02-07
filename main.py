50877dd8bf1458331782e4b2636c98507a894e56
JianGoersch <107126158+JianGoersch@users.noreply.github.com>
2024-02-07 09:04:11 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-# import locale
 from streamlit_extras.metric_cards import style_metric_cards
 import plotly as pl
 import plotly.express as px
